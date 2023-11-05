b12af004a679beb643a31c8c3da274dddc4a985c
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-11-05 01:05:50 -0300

    problema con la piramide

---
+++ js/ejercicio6.js
@@ -0,0 +1,18 @@
+/*
+6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+*/
+
+for (let i = 1; i <= 30; i++) {
+    let linea = "";
+    for (let b = 1; b <= i; b++) {
+      linea += i;
+    }
+    document.write(linea + '<br>');
+  }
