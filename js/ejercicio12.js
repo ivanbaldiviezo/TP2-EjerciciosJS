82e3fa238c5c175117064efcf1016676cd08d98e
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-11-07 00:47:42 -0300

    Se realizo ejercicio 12 y se resuelve duda para que el numero aleatorio sea un numero entero con Math.floor

---
+++ js/ejercicio12.js
@@ -0,0 +1,5 @@
+// 12- Realiza un script que genere un número aleatorio entre 1 y 99
+
+const numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+
+document.write(`Número aleatorio entre 1 y 99: ${numeroAleatorio}`);
